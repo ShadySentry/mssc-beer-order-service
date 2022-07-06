7dabd13bb56f24d0551ecdd89a1beeffc5c8930a
ShadySentry <eurohawk@gmail.com>
2022-07-06 11:42:39 +0300

    added jms config

---
+++ src/main/java/guru/sfg/beer/order/service/config/JmsConfig.java
@@ -0,0 +1,24 @@
+package guru.sfg.beer.order.service.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jms.support.converter.MappingJackson2MessageConverter;
+import org.springframework.jms.support.converter.MessageConverter;
+import org.springframework.jms.support.converter.MessageType;
+
+@Configuration
+public class JmsConfig {
+
+    public static final String BEER_SERVICE_QUEUE="beer-service";
+    public static final String BEER_ORDER_SERVICE_QUEUE="beer-order-service";
+    public static final String BEER_INVENTORY_SERVICE_QUEUE="beer-inventory-service";
+
+    @Bean
+    public MessageConverter messageConverter(){
+        MappingJackson2MessageConverter converter = new MappingJackson2MessageConverter();
+        converter.setTargetType(MessageType.TEXT);
+        converter.setTypeIdPropertyName("_type");
+
+        return converter;
+    }
+}
